55805fb2484ad710b8a2bd0156cfc26ed835cc5d
Mohanad Elsafty <mohan3d94@gmail.com>
2022-07-27 19:53:46 +0800

    chor: bump version to 0.3.0

--- downloader.py
+++ downloader.py
@@ -37,7 +37,7 @@ import requests
 
 __author__ = "mohan3d"
 __author_email__ = "mohan3d94@gmail.com"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 ARABIC_TOONS_HOST = 'www.arabic-toons.com'
 ARABIC_TOONS_USER_AGENT = {
